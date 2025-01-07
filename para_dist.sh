e58b4e84e0a067548e8038da8e1cc810e532d85e
Nan Ouyang <73474781+ouyangnann@users.noreply.github.com>
2025-01-07 20:06:02 +0800

    Update para_dist.sh

--- para_dist.sh
+++ para_dist.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 python main_dist_para.py --models itpgtn --config_filename  data/dist_config/dist_itpgtn_metr.yaml --load_pretrained --cuda cuda:0
-=======
-python main_dist_para.py --models itpgtn --config_filename  data/dist_config/dist_itpgtn_pems04.yaml --load_pretrained --cuda cuda:0
->>>>>>> 5ad63e5847f13f0b0c5a4e5520c183e4bf9c2c70
